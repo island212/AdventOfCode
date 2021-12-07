e5b98c8890c46bc02627d1fe2da96ee60eb774b6
island212 <islandx424@gmail.com>
2021-12-06 20:44:27 -0500

    Day 1 puzzle

---
+++ 2021/day1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Input.txt"));
+            int[] report = (Array.ConvertAll(input, s => Int32.Parse(s)));
+
+            Console.WriteLine("########## Day 1 2020 ##########");
+            Console.WriteLine($"Part one solution: {SolvePartOne(report)}");
+            Console.WriteLine($"Part two solution: {SolvePartTwo(report)}");
+            Console.WriteLine("################################");
+        }
+
+        static int SolvePartOne(int[] report)
+        {
+            int counter = 0;
+
+            int last = report[0];
+            for (int i = 1; i < report.Length; i++)
+            {
+                int current = report[i];
+                if (current > last)
+                    counter++;
+                last = current;
+            }
+            return counter;
+        }
+
+        static int SolvePartTwo(int[] report)
+        {
+            int counter = 0;
+
+            int last = report[0] + report[1] + report[2];
+            for (int i = 1; i < report.Length - 2; i++)
+            {
+                int current = report[i] + report[i + 1] + report[i + 2];
+                if (current > last)
+                    counter++;
+                last = current;
+            }
+            return counter;
+        }
+    }
+}
